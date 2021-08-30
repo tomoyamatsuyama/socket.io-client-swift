fa453bd9cba66a7168837252227eca271adfcd57
tomoyamatsuyama <m.tomoya0221@gmail.com>
2021-08-30 23:42:11 +0900

    Make SocketIO dir and add target

--- Package.swift
+++ Package.swift
@@ -3,5 +3,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "SocketIO"
+    name: "SocketIO",
+    products: [
+        .library(name: "SocketIO", targets: ["SocketIO"])
+    ],
+    targets: [
+        .target(name: "SocketIO")
+    ]
 )
